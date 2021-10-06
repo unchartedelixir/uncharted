8485ac2dbc91a43ae95ab68806681235207f0e30
Stamates <Stamates@users.noreply.github.com>
2021-10-06 13:08:49 -0400

    Setup doughnut ring and add internal value/labels

--- uncharted/lib/uncharted/doughnut_chart/dataset.ex
+++ uncharted/lib/uncharted/doughnut_chart/dataset.ex
@@ -2,9 +2,22 @@ defmodule Uncharted.DoughnutChart.Dataset do
   @moduledoc """
   Struct representing a dataset for a basic doughnut chart.
   """
-  defstruct [:data]
+  defstruct [
+    :data,
+    :center_value,
+    :center_value_fill_color,
+    :label,
+    :label_fill_color,
+    :secondary_label
+  ]
 
+  @typep color :: atom()
   @type t() :: %__MODULE__{
-          data: Uncharted.DoughnutChart.Dataset.t()
+          data: Uncharted.DoughnutChart.Dataset.t(),
+          center_value: number(),
+          center_value_fill_color: color(),
+          label: String.t() | nil,
+          label_fill_color: color(),
+          secondary_label: String.t() | nil
         }
 end
